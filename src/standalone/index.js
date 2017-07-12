424f7a816bc332e40067d9fb8c13bbd8a53cd7ba
Andrei Belov <defanator@gmail.com>
2017-07-12 13:19:12 +0300

    Remove Topbar plugin from standalone layout
    See also:
    https://github.com/swagger-api/swagger-ui/issues/3229

--- src/standalone/index.js
+++ src/standalone/index.js
@@ -1,13 +1,11 @@
 import StandaloneLayout from "./layout"
 import "../style/main.scss"
 
-import TopbarPlugin from "plugins/topbar"
 import ConfigsPlugin from "plugins/configs"
 
 // the Standalone preset
 
 let preset = [
-  TopbarPlugin,
   ConfigsPlugin,
   () => {
     return {
